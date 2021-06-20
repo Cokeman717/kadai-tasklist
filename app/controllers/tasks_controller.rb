a76f67260c1461ccf18ce71e7f2b6bc6a360ec43
cokeman <cokeredder@gmail.com>
2021-06-20 15:39:52 +0000

    Tasklist login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,9 @@
 class TasksController < ApplicationController
+    before_action :require_user_logged_in
+    before_action :correct_user, only: [:show, :edit, :destroy]
+    
     def index
+        @tasks = current_user.tasks
     end
     
     def show
@@ -7,11 +11,11 @@ class TasksController < ApplicationController
     end
     
     def new
-        @task = Task.new
+        @task = current_user.tasks.build
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             flash[:success] = 'タスクが正常に追加されました'
@@ -50,5 +54,12 @@ class TasksController < ApplicationController
     
     def task_params
         params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+        @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+            redirect_to root_url
+        end
     end
 end
---
+++ db/migrate/20210620133523_removeuser_fromtasks.rb
@@ -0,0 +1,4 @@
+class RemoveuserFromtasks < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
